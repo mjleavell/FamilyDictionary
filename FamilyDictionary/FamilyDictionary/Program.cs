a5fc652916e1b8bd1ca21daf0693ac3ec210ddf1
Maggie Leavell <mjleavell@gmail.com>
2019-03-12 20:29:14 -0500

    completed step 2

--- FamilyDictionary/FamilyDictionary/Program.cs
+++ FamilyDictionary/FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,24 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. Define a Dictionary that contains information about several members 
+            // of your family. Use the following example as a template.
+            // EXAMPLE: Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            // myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+
+            Dictionary<string, Dictionary<string, string>> LeavellFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            LeavellFamily.Add("twin", new Dictionary<string, string>() { { "name", "Neely" }, { "age", "25" } });
+            LeavellFamily.Add("sister", new Dictionary<string, string>() { { "name", "Lee" }, { "age", "28" } });
+
+            // 2. Next, iterate over each item in myFamily and produce the following output.
+            // EXAMPLE: Krista is my sister and is 42 years old
+            foreach (var (key, value) in LeavellFamily)
+            {
+                Console.WriteLine($"{LeavellFamily[key]["name"]} is {LeavellFamily[key]["age"]}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
